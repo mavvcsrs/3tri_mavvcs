9417f78a13fc0c13eae16dd616fa621b391b4148
mavvi <114171956+mavvcsrs@users.noreply.github.com>
2024-10-10 12:40:41 +0000

    ALTERAÇOES

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,7 +3,16 @@ const url = ''https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt (dados.tempo_medio - horas) *100)
+    const minutos = Math.round(pessoasConectadas / pessoasNoMundo) *100).toFixed(2)
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = 'Você sabia que o mundo tem'
+    PAREI EM AULA 7
+
 }
 
 vizualizarInformacoesGlobais()
